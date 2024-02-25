262d693a200d37279db694ea99a40ac43bdeb340
Erkin işçi <erkin.isci@gmail.com>
2024-02-25 16:40:31 +0100

    feat: add openTelemetry & jaeger exporter

--- src/Orders.Api/Program.cs
+++ src/Orders.Api/Program.cs
@@ -1,13 +1,22 @@
+using System.Diagnostics;
 using System.Reflection;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using MassTransit;
+using MassTransit.Metadata;
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Orders.Api;
+using Orders.Api.Controllers;
 using Orders.Api.Data;
 using Orders.Api.Repositories;
 using Orders.Api.Services;
 using Orders.Api.Validation;
+using Serilog;
+using Serilog.Events;
+
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
@@ -26,6 +35,44 @@ builder.Services.AddDbContext<AppDbContext>(x =>
     });
 });
 
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("MassTransit", LogEventLevel.Debug)
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+builder.Services.AddOpenTelemetry().WithTracing(x =>
+{
+    x.SetResourceBuilder(ResourceBuilder.CreateDefault()
+            .AddService("orders-api")
+            .AddTelemetrySdk()
+            .AddEnvironmentVariableDetector())
+        .AddSource("MassTransit")
+        .AddSource(nameof(OrderController))
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddOtlpExporter(o =>
+        {
+            var host = HostMetadataCache.IsRunningInContainer ? "jaeger" : "localhost";
+            var url = $"http://{host}:6831";
+            o.Endpoint = new Uri(url);
+            o.ExportProcessorType = ExportProcessorType.Batch;
+            o.BatchExportProcessorOptions = new BatchExportProcessorOptions<Activity>
+            {
+                MaxQueueSize = 2048,
+                ScheduledDelayMilliseconds = 5000,
+                ExporterTimeoutMilliseconds = 30000,
+                MaxExportBatchSize = 512,
+            };
+        });
+});
+
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
@@ -41,7 +88,8 @@ builder.Services.AddMassTransit(x =>
     x.AddEntityFrameworkOutbox<AppDbContext>(o =>
     {
         o.QueryDelay = TimeSpan.FromSeconds(1);
-        o.UsePostgres().UseBusOutbox();
+        o.UsePostgres()
+            .UseBusOutbox();
     });
     
     x.UsingAzureServiceBus((ctx, cfg) =>
